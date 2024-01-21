f74d1bc8fc3dee6ffb342aff3d18dc24539c1b8a
kajal.singh <kajalsingh1813@gmail.com>
2024-01-21 12:59:06 +0530

    Feat: Added the edit feature

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,42 +1,54 @@
 import Popup from 'reactjs-popup';
 import {useState} from 'react';
 import Tasks from "./Tasks"
+
 function Add() {
     const [message,setMessage]= useState("");
     const [list,setList]= useState([]);
+
     const handleChange = event => {
        setMessage(event.target.value);
     }
+
     const handleClick = () => {
         setList((prevList)=>[...prevList,message]);
     }
+
     const delClick = (i) => {
          const newList = list.filter((item,index)=> index!==i);
          setList(newList);
     }
 
+    const editClick = (index,updatedValue) => {
+          const updatedList = [...list];
+          updatedList[index]= updatedValue;
+          setList(updatedList);
+      };
+
     return (
         <div>
-            <Popup trigger={ 
-            <button className="add-button">Add+</button>}
-            modal
-            >
+            <Popup trigger={<button className="add-button">Add+</button>}>
                 {(close)=> (
                     <div className="pop-up">
                     <textarea className="pop-text" placeholder='Enter your feelings....' onChange={handleChange}></textarea>
                     <button className="pop-button" onClick={()=> {handleClick(); close();}} >save</button>
                    </div>
                 )}
-
             </Popup>
            <div className="list">
             <div>
-                List of Items
+                List of Notes
             </div>
             {
             list.map((item,index)=> 
             <>
-            <Tasks key={index} name={item} onClick = {()=>delClick(index)}/>
+            <Tasks
+                key={index} 
+                name={item}
+                value={index}
+                onClick = {()=>delClick(index)} 
+                editClick={editClick} 
+             />
             </>)
            }
            </div>
@@ -46,7 +58,3 @@ function Add() {
 }
 
 export default Add;
-
-
-
-
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,22 +1,35 @@
-
+import {useState} from "react";
 import Popup from "reactjs-popup";
 function Tasks(props) {
 
-    const name= props.name;
-    const onClick = props.onClick;
-   const index=props.index;
-
+    const { name, onClick, editClick, value } = props;
+    const [neww,setNeww] = useState(name);
+    const handleChange= (e)=> {
+        setNeww(e);
+   }
+      
     return (
         <div className="tasks">
             <div className="tasks-n">
-            {name}
+                {name}
             </div>
-            <div>
-            <button className="del"  onClick = {onClick}>Delete</button>
-       </div>
-            
+            <div className="buttons">
+                <Popup trigger = {<button className="del" >Edit</button>}>
+                    {
+                        (close)=> (
+                                <div className="pop-up">
+                                    <textarea className="pop-text" key={value} value={neww}  onChange= {(e)=> handleChange(e.target.value) }></textarea>
+                                    <button className="pop-button" onClick={()=> {editClick(value,neww);close();}}>Save</button>
+                                </div>
+                                )
+                    }
+                </Popup>
+                <button className="del"  onClick = {onClick}>Delete</button>
+            </div>
         </div>
     )
 }
 
 export default Tasks;
+
+
